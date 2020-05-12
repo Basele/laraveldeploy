bc79d790329cb1d4b1291c46fe5c8690a67b0a08
Basele Stephen <s.basele@gmail.com>
2020-05-12 19:12:55 +0300

    Logo Added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
             <div class="container">
-                <a href="#page-top"><img class="navbar-img" src="{{ asset('HomeTheme/assets/img/Aidpedia.png') }}"></a>
+                <a href="#page-top"><img class="navbar-img" src="{{ asset('HomeTheme/assets/img/AidPedia.png') }}"></a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu<i class="fas fa-bars ml-1"></i></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav text-uppercase ml-auto">
