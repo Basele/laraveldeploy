74cf29bc7eebef2564c4d63de0511821b9f6678d
Basele Stephen <s.basele@gmail.com>
2020-05-12 19:27:43 +0300

    Title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'AidPedia') }}</title>
 
     <link rel="icon" type="image/x-icon" href="{{ asset('HomeTheme/assets/img/favicon.ico') }}" />
     <!-- Font Awesome icons (free version)-->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    AidPedia
                 </div>
 
                 <div class="links">
